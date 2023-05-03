f3937e1649526832af47f8a9f7ddeb9136b096ef
Snooopyman <armando.snoop@gmail.com>
2023-05-03 17:42:39 +0200

    updateUI - changing question of quiz each time you press your answer

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let quiz = [
+        "2 + 2  = 4 ?",
+        "2 * 2  = 8 ?",
+        "2 - 2  = 4 ?"
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI () {
+        questionLabel.text = quiz[questionNumber]
+    }
+    
 }
 
